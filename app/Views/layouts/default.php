3c937082bb9dd86ff2fb1c8c24f1dc7dd0ee11eb
iwanf <firmawaneiwan@gmail.com>
2021-04-18 22:08:28 +0700

    create offline assets for js and css

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha512-9BwLAVqqt6oFdXohPLuNHxhx36BVj5uGSGmizkmGkgl3uMSgNalKc/smum+GJU/TTP0jy0+ruwC3xNAk3F759A==" crossorigin="anonymous" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.24/datatables.min.css"/>
+    <link href="<?= base_url()?>/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="<?= base_url()?>/font-awesome/v5/css/all.min.css"  />
+    <link rel="stylesheet" type="text/css" href="<?= base_url()?>/css/datatables.min.css"/>
     <title>SPTB - <?= $title ?? '' ?></title>
 </head>
 
--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -6,9 +6,9 @@
     </p>
   </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.24/datatables.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+<script src="<?= base_url()?>/js/jquery-3.6.0.min.js" ></script>
+<script type="text/javascript" src="<?= base_url()?>/js/datatables.min.js"></script>
+<script src="<?= base_url()?>/js/bootstrap.bundle.min.js" ></script>
 <script>
 $(document).ready(function() {
     $('#dataTables').DataTable( {
